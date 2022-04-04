3ace33eeef86967f535636367012d831ec9e142c
fara-android <abdusatarovfaruh3@gmail.com>
2022-04-04 16:17:30 +0600

    main_screen. WIP

--- lib/features/presentation/screens/main/main_screen/main_screen.dart
+++ lib/features/presentation/screens/main/main_screen/main_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:wallet_ui/core/styles.dart';
+import 'package:wallet_ui/features/presentation/screens/main/main_screen/components/custom_finance_container.dart';
+import 'package:wallet_ui/features/presentation/screens/main/main_screen/components/custom_profile_info_field.dart';
+import 'package:wallet_ui/features/presentation/screens/main/main_screen/components/favorites_custom_banner_container.dart';
+import 'package:wallet_ui/features/presentation/screens/main/main_screen/components/favorites_custom_circle_avatar.dart';
 import 'package:wallet_ui/features/presentation/screens/main/main_screen/components/custom_fast_action_container.dart';
+import 'package:wallet_ui/features/presentation/widgets/custom_button.dart';
+import 'package:wallet_ui/features/presentation/widgets/scale_animated_container.dart';
 
 part 'part/home_appbar.dart';
 part 'part/fast_balance_actions.dart';
 part 'part/balance.dart';
+part 'part/favorites.dart';
+part 'part/my_finance.dart';
+part 'part/ad_container.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -12,9 +21,10 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Styles.backgroundColor2,
+      backgroundColor: Styles.brandBlue,
       body: SafeArea(
-        child: Column(
+        child: ListView(
+          physics: ClampingScrollPhysics(),
           children: [
             Container(
               padding: EdgeInsets.symmetric(horizontal: 16, vertical: 22),
@@ -29,8 +39,19 @@ class MainScreen extends StatelessWidget {
               ),
             ),
             Container(
-              color: Colors.white,
-            )
+              color: Styles.backgroundColor,
+              child: Column(
+                children: [
+                  SizedBox(height: 32),
+                  Favorites(),
+                  SizedBox(height: 32),
+                  AdContainer(),
+                  SizedBox(height: 32),
+                  Description(),
+                  SizedBox(height: 32)
+                ],
+              ),
+            ),
           ],
         ),
       ),
